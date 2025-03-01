25b234e4791fb4d718adf458a343f01c4a86bde1
Charan Polisetty <charan.ssc006@gmail.com>
2025-03-01 14:34:12 +0530

    created Login and signup components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import "./App.css";
-import Drawer from "./components/Drawer/drawer";
-import { Header } from "./components";
+import Drawer from "./components/Drawer/drawer"
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import SignUp from "./components/Signup/signup";
 
-const theme = createTheme(); 
+const theme = createTheme();
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Drawer />
-      </div>
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/"
+          element={
+            <ThemeProvider theme={theme}>
+              <div className="App">
+                <Drawer />
+              </div>
+            </ThemeProvider>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
